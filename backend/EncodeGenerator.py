1f20f65f47c58b91741f14e19de61354e7a929dc
TejasPatne <tejaspatne18@gmail.com>
2024-02-20 22:11:46 +0530

    upload image to firebase storage

--- backend/EncodeGenerator.py
+++ backend/EncodeGenerator.py
@@ -2,6 +2,18 @@ import cv2
 import face_recognition
 import pickle
 import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://facerecognitionrealtime-fcb19-default-rtdb.firebaseio.com/",
+    'storageBucket': "facerecognitionrealtime-fcb19.appspot.com"
+})
+
+bucket = storage.bucket()
 
 # Importing student images
 folderPath = 'Images'
@@ -13,6 +25,11 @@ studentIds = []
 for path in pathList:
     imgList.append(cv2.imread(os.path.join(folderPath, path)))
     studentIds.append(os.path.splitext(path)[0])
+
+    fileName = f'{folderPath}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
 # print(studentIds)
 
 def findEncodings(imagesList):
