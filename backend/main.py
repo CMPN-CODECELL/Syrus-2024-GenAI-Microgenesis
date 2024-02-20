e7fb5ceb7849466b639d590c25faf8850457f0f1
TejasPatne <tejaspatne18@gmail.com>
2024-02-20 15:52:28 +0530

    face detection

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
 import cv2
 import os 
+import pickle
+import face_recognition
+import numpy as np
+import cvzone
 
 # Open the default camera (index 0)
 cap = cv2.VideoCapture(0)
@@ -18,10 +22,49 @@ imgModeList = []
 for path in modePathList:
     imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
 
+# Load the encoding file
+print("Loading Encode File ...")
+file = open('EncodeFile.p', 'rb')
+encodeListKnownWithIds = pickle.load(file)
+file.close()
+encodeListKnown, studentIds = encodeListKnownWithIds
+# print(studentIds)
+print("Encode File Loaded")
+
 # Loop to continuously read frames from the camera
 while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    try:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Check if the frame was captured successfully
+        if not ret:
+            print("Error: Failed to capture frame")
+
+        imgS = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    except:
+        print("Error: Failed to capture/resize frame")
+
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+
+    for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print("matches", matches)
+        # print("faceDis", faceDis)
+
+        matchIndex = np.argmin(faceDis)
+        # print("Match Index", matchIndex)
+
+        if matches[matchIndex]:
+            # print("Known Face Detected")
+            # print(studentIds[matchIndex])
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            bbox = 15 + x1, 72 + y1, x2 - x1, y2 - y1
+            frame = cvzone.cornerRect(frame, bbox, rt=0)
 
     # Check if the frame was captured successfully
     if not ret:
